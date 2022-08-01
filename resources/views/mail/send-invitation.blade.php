ce605b8830b379ee48640c7236802ae3d785af91
nabeelirbab <nabeelirbab@gmail.com>
2022-08-01 08:57:25 +0500

    invite setting

--- resources/views/mail/send-invitation.blade.php
+++ resources/views/mail/send-invitation.blade.php
@@ -3,7 +3,7 @@
 
 To accept the invitation, simply click the link below: 
 <br>
-<a href="{{url('users/register?invite=invited')}}" style="font-size:16px;font-family:'Lato',Helvetica,Arial,sans-serif;color:#ffffff;text-decoration:none;color:#ffffff;text-decoration:none;border-radius:3px;padding:10px 15px;border:1px solid #48a7fe;display:block;text-align:center;background-color:#48a7fe;width: 32%;margin-top: 12px" >
+<a href="{{url('users/register?invite='.$maildata['code'])}}" style="font-size:16px;font-family:'Lato',Helvetica,Arial,sans-serif;color:#ffffff;text-decoration:none;color:#ffffff;text-decoration:none;border-radius:3px;padding:10px 15px;border:1px solid #48a7fe;display:block;text-align:center;background-color:#48a7fe;width: 32%;margin-top: 12px" >
 Accept Invitation
 </a>
 <br>
