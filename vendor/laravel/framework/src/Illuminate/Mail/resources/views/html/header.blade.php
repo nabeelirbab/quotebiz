e5e8aea77feabe5e29e85215129136e704b66adf
nabeelirbab <nabeelirbab@gmail.com>
2022-08-12 07:28:00 +0500

    change email logo

--- vendor/laravel/framework/src/Illuminate/Mail/resources/views/html/header.blade.php
+++ vendor/laravel/framework/src/Illuminate/Mail/resources/views/html/header.blade.php
@@ -1,11 +1,11 @@
 <?php
- $sitelargelogo = action('SettingController@file', \Acelle\Model\Setting::get('site_logo_big'));
+ $sitelargelogo = action('SettingController@file', \Acelle\Model\Setting::get('site_logo_small'));
  ?>
 <tr>
 <td class="header">
 <a href="{{ url('') }}" style="display: inline-block;">
-
-<h4>{{\Acelle\Model\Setting::get('site_name')}}</h4>
+<!-- <h4>{{\Acelle\Model\Setting::get('site_name')}}</h4> -->
+<img src="{{ $sitelargelogo }}">
 
 </a>
 </td>
