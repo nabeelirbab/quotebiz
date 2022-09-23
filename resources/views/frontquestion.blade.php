276fe50df05c31a3cb887cb1d02ce5a852d30771
nabeelirbab <nabeelirbab@gmail.com>
2022-09-23 09:35:00 +0500

    changessssssssss

--- resources/views/frontquestion.blade.php
+++ resources/views/frontquestion.blade.php
@@ -329,7 +329,7 @@
                          <div class="form-row">
 	                    	<div class="form-holder">
 	                    		<!-- <span>{{$question->question}}</span> -->
-	                    		<input type="text" class="form-control zipclass" name="input[]" placeholder="{{$question->question}}" autocomplete="new-password[]" required>
+	                    		<input type="number" class="form-control zipclass" name="input[]" placeholder="{{$question->question}}" autocomplete="new-password[]" required>
 	                    	</div>
 	                    </div>
 	                </div>
