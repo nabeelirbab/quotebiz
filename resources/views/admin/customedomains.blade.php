16a56ced2453c5c251d4c4ee839518a090d97f4a
nabeelirbab <nabeelirbab@gmail.com>
2023-01-04 08:24:02 +0500

    update

--- resources/views/admin/customedomains.blade.php
+++ resources/views/admin/customedomains.blade.php
@@ -20,7 +20,7 @@
 <div class="nk-block-head nk-block-head-sm">
 <div class="nk-block-between">
 <div class="nk-block-head-content">
-<h3 class="nk-block-title page-title">Customer Lists</h3>
+<h3 class="nk-block-title page-title">Domains List</h3>
 <div class="nk-block-des text-soft">
    
 </div>
