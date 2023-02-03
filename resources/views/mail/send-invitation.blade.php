175b20b7fe65636fefe14d14e8ec03f915a1a62f
nabeelirbab <nabeelirbab@gmail.com>
2023-02-03 09:56:03 +0500

    email text change

--- resources/views/mail/send-invitation.blade.php
+++ resources/views/mail/send-invitation.blade.php
@@ -3,12 +3,18 @@
 <p style="font-family: DM Sans, sans-serif;"> Hi {{$maildata['name']}} </p>
 <p style="font-size: 17px;margin: 0;font-family: DM Sans, sans-serif;">
 {{ \Acelle\Model\Setting::get('site_name') }} would like to invite you to join their quote business network. You can subscribe to notifications about job opportunities that come through our system and provide a quote if you'd like the job. 
+</p>
 <br>
+<p style="font-size: 17px;margin: 0;font-family: DM Sans, sans-serif;">
 A maximum of 3 service providers are able to quote each job, so it pays to act quickly.
+</p>
 <br>
+<p style="font-size: 17px;margin: 0;font-family: DM Sans, sans-serif;">
 You get 100 free credits when you sign up! 
+</p>
 <br>
 <br>
+<p style="font-size: 17px;margin: 0;font-family: DM Sans, sans-serif;">
 To accept the invitation, simply click the link below: 
 </p>
 <br>
