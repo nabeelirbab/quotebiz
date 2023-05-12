91bbaaa248eab925c3144bcdac4d7b1b6f1af9bc
nabeelirbab <nabeelirbab@gmail.com>
2023-05-12 17:39:46 +0500

    featured blogs and footer

--- resources/views/blogShow.blade.php
+++ resources/views/blogShow.blade.php
@@ -12,7 +12,9 @@
     max-width:100%; /* adjust this value as needed */
     overflow-y: auto;
 }
-
+.container, .container-sm {
+    max-width: 100%;
+}
 </style>
 @endsection
 
@@ -25,8 +27,8 @@
 
 <div class="nk-block">
 <div class="row g-gs">
-<div class="row">
-  <div class="col-md-12 text-center">
+<div class="row justify-content-center">
+  <div class="col-lg-9 col-sm-12 text-center">
     <h1>{{ $post->title }}</h1>
     <p class="text-muted">{{ $post->created_at->format('M j, Y') }}</p>
     <img src="{{ asset('frontend-assets/images/posts/' . $post->cover_img) }}" class="img w-50 mb-4" alt="{{ $post->title }}">
